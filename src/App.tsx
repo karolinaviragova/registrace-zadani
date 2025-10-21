8a9ba67f6b239bf3ce6a7eb757c7d2d5d793a2b0
Luděk Roleček <ludek@rolecek.cz>
2025-10-21 16:37:37 +0200

    Update to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 export const App = () => {
   return (
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>Webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu.
-        </p>
-      </main>
+      <h1>Registrace</h1>
     </div>
   );
 };
